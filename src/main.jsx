12512ca42f2119fc8f56df8981ef894c3ef449a8
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-07-26 10:35:38 +0530

    fix: correct navbar import path and adjust Hero component comment

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './Components/Navber.jsx'
+// import Hero from './Components/Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='bg-white'>
     <Navbar />
+    {/* <Hero/> */}
     </div>
   </StrictMode>,
 )
