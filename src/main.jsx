91c87affc3fd63e6cf12e89d24334b8634f441cc
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-07-27 21:22:51 +0530

    added the banner

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './Components/Navber.jsx'
-// import Hero from './Components/Hero.jsx'
+import Hero from './Components/Hero.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='bg-white'>
+    <div className=' bg-gray-300 px-2 '>
     <Navbar />
-    {/* <Hero/> */}
+    <Hero/>
+    <div classname="pt-5" >
+      <div className="relative w-full   rounded-2xl  border-2 border-[#e7c99b]">
+
+  <img
+    src="Assets/banner.jpg"
+    alt="Banner"
+    className="w-full h-[30vh] object-cover object-left rounded-2xl"
+  />
+
+ 
+  <div className="absolute inset-0 bg-gradient-to-r from-[#ffba52] to-white opacity-50"></div>
+</div>
+
+      </div>
     </div>
   </StrictMode>,
 )
