2b51a86f4f5245e7baf07e81be95469e7a0a092e
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-07-27 21:23:09 +0530

    fixed the data extension

---
+++ src/Utility/data.js
@@ -0,0 +1,33 @@
+export const slides = [
+
+  {
+     img: "Assets/hero/img1.jpg",
+    title: "",
+    topic: "",
+    des: "",
+    buttons: [],
+    thumbnailtitle: "",
+    thumbnaildesc: "",
+    path: "/Workshops",
+  },
+ {
+     img: "Assets/hero/img2.jpg",
+    title: "",
+    topic: "",
+    des: "",
+    buttons: [],
+    thumbnailtitle: "",
+    thumbnaildesc: "",
+    path: "/Workshops",
+  },
+//  {
+//      img: "Assets/hero/img3.png",
+//     title: "",
+//     topic: "",
+//     des: "",
+//     buttons: [],
+//     thumbnailtitle: "",
+//     thumbnaildesc: "",
+//     path: "/Workshops",
+//   },
+];
