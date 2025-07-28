1eb2d07993f3477818fc1a87576855776dc109c4
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-07-28 10:42:57 +0530

    added styles to the banner

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Navbar from './Components/Navbar.jsx'
-import Hero from './Components/Hero.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Navbar from "./Components/Navbar.jsx";
+import Hero from "./Components/Hero.jsx";
+import Products from "./Components/Product.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className=' bg-gray-300 px-2 '>
-    <Navbar />
-    <Hero/>
-    <div classname="pt-5" >
-      <div className="relative w-full   rounded-2xl  border-2 border-[#e7c99b]">
+    <div className=" bg-gray-300 px-2 ">
+      <Navbar />
+      <Hero />
+      <section className="pb-4">
+        <div className="relative w-full   rounded-2xl  border-2 border-[#e7c99b] ">
+          <img
+            src="Assets/banner.jpg"
+            alt="Banner"
+            className="w-full h-[30vh] object-cover object-left rounded-2xl"
+          />
 
-  <img
-    src="Assets/banner.jpg"
-    alt="Banner"
-    className="w-full h-[30vh] object-cover object-left rounded-2xl"
-  />
-
- 
-  <div className="absolute inset-0 bg-gradient-to-r from-[#ffba52] to-white opacity-50"></div>
-</div>
-
-      </div>
+          <div className="absolute inset-0 bg-gradient-to-r from-[#ffba52] to-white opacity-50 rounded-2xl ">
+            <h1 className="text-center pt-20 text-5xl text-black font-cursive">
+              Light your life with our candles
+            </h1>
+          </div>
+        </div>
+      </section>
+      <Products/>
     </div>
-  </StrictMode>,
-)
+  </StrictMode>
+);
