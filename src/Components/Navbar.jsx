de2308af6a8b30efdb6eebaff466589b34663ac3
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-07-30 22:15:05 +0530

    changed the sec tag into div for testing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,8 @@ import candle from "/Assets/logo/candle.png"
 export default function Header(){
    const [toggleMenu,settoggleMenu]= useState(false);
     return <>
-    <section className=" absolute top-0 left-0 w-full z-50 px-3 pt-3  cursor-default  ">    
+    
+    <div className=" absolute top-0 left-0 w-full z-50 px-3 py-3  cursor-default  ">    
     <header className="flex justify-between px-10 py-3.5  rounded-2xl border-2  border-[#fcf5f5] shadow-2xl bg-[#f1cc94]">
         
         <img src={candle} alt="logo" className='border-r-4 border-[#fcfaf7] h-14 w-18  pr-4  ' />
@@ -17,8 +18,8 @@ export default function Header(){
         <ul className=" flex gap-9  mt-2 text-gray-600 font-mono text-2xl  text-left">
            <li> <a href="#products" className='pb-1 border-b-4  border-[#8a673d] hover:border-white  transition-all duration-300'>Products</a></li>
             <li><a href="#project" className='pb-1 border-b-4 border-[#8a673d] hover:border-white transition-all duration-300'>Cart</a></li>
-            <li><a href="#project" className='pb-1 border-b-4 border-[#8a673d] hover:border-white transition-all duration-300'>Faq</a></li>
-            <li><a href="#resume" className='pb-1 border-b-4 border-[#8a673d] hover:border-white transition-all duration-300'>About</a></li>
+            <li><a href="#Faq" className='pb-1 border-b-4 border-[#8a673d] hover:border-white transition-all duration-300'>Faq</a></li>
+            <li><a href="#About" className='pb-1 border-b-4 border-[#8a673d] hover:border-white transition-all duration-300'>About</a></li>
             <li><a href="#contact" className='pb-1 border-b-4 border-[#8a673d] hover:border-white transition-all duration-300'>Contact</a></li>
         </ul>
         </nav> 
@@ -41,6 +42,7 @@ export default function Header(){
         <button onClick={()=>settoggleMenu(!toggleMenu)} className='block md:hidden'> <Bars3Icon className=' text-black h-10 pb-2 '/></button>
         
     </header>
-    </section>
+    </div>
+   
     </>
 }
