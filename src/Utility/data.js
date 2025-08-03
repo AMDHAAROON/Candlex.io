11cb4175af69180969575ed845b855781f61821d
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-08-03 21:19:34 +0530

    Added the data for new components

--- src/Utility/data.js
+++ src/Utility/data.js
@@ -34,16 +34,80 @@ export const slides = [
 
 export const candleProducts = [
   {
-    name: "Vanilla Bliss",
-    image: "Assets/seperate/p3.png"
+    name: "Morroce Gift set -Artic Breeze",
+    image: "Assets/seperate/p3.jpeg"
   },
   {
-    name: "Rose Garden",
-    image: "Assets/seperate/p2.png"
+    name: "Magical Dunes -Glass jar",
+    image: "Assets/seperate/p2.jpeg"
   },
   {
-    name: "Magical dunes",
-    image: "Assets/seperate/p1.png"
+    name: "Magrble bass with glass holder",
+    image: "Assets/seperate/p1.jpeg"
   },
   // Add more products...
+];
+
+export const shop = [
+  {
+   
+    image: "Assets/seperate/s1.png"
+  },
+  {
+   
+    image: "Assets/seperate/s2.png"
+  },
+  {
+   
+    image: "Assets/seperate/s3.png"
+  },
+  // Add more products...
+];
+
+export const categories = [
+  {
+   
+    image: "Assets/seperate/c1.png",
+    name: "Burano Candles"
+  },
+  {
+   
+    image: "Assets/seperate/c2.png",
+    name: "Glass Candles"
+  },
+  {
+    image: "Assets/seperate/c3.png",
+    name: "Car perfumes"
+
+  },
+    {
+    image: "Assets/seperate/c4.png",
+    name: "Candle Accessories"
+
+  },
+  // Add more product
+  //s...
+];
+
+export const Collections = [
+  {
+   
+    image: "Assets/hero/big1.png",
+    name: "Marble Black & White Tea Light Holder"
+  },
+   {
+   
+    image: "Assets/hero/big2.png",
+    name: "Marble Standing Candle Holder - Set"
+  },
+  // {
+  //   image: "Assets/seperate/c3.png",
+  //   name: "Car perfumes"
+
+  // },
+  //   {
+  //   image: "Assets/seperate/c4.png",
+  //   name: "Candle Accessories"
+  // },
+  // // Add more products...
 ];
