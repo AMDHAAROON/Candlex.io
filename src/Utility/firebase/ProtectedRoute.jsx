6fdbfcae0d980251fa5ec72ffcc581bffa8d5d1b
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-08-12 21:58:42 +0530

    moved: moved the firebase.js and protectedroute into a seperate folder called firebase

--- src/Utility/firebase/ProtectedRoute.jsx
+++ src/Utility/firebase/ProtectedRoute.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../Utility/firebase"; // your firebase config
+import { auth } from "./firebase"; // your firebase config
 
 export default function ProtectedRoute({ children }) {
   const [user, loading] = useAuthState(auth);
