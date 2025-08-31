b2a4ef2c7c8d33174e5402229a641b9da1161630
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-08-31 22:39:21 +0530

    Refactor : implemented responsiveness in mobile view

--- src/Components/banner.jsx
+++ src/Components/banner.jsx
@@ -2,18 +2,18 @@ import { useInView } from "react-intersection-observer";
 
 const BannerSection = () => {
   const { ref, inView } = useInView({
-    threshold: 0.3, // section is considered visible when 30% is in view
+    threshold: 0.8, // section is considered visible when 30% is in view
     triggerOnce: false,
   });
 
   return (
     <section className="pb-4" ref={ref} id="banner">
-      <div className="relative w-full rounded-2xl border-2 border-[#e7c99b] overflow-hidden  shadow-xl">
+      <div className="relative w-full rounded-2xl border-2 border-[#e7c99b] overflow-hidden shadow-xl">
         {/* Banner Image */}
         <img
           src="Assets/banner.jpg"
           alt="Banner"
-          className="w-full h-[30vh] object-cover object-left rounded-2xl"
+          className="w-full h-[10vh] sm:h-[50vh] md:h-[30vh] object-cover object-left rounded-2xl"
         />
 
         {/* Gradient Overlay */}
@@ -25,11 +25,13 @@ const BannerSection = () => {
 
         {/* Text */}
         <div
-          className={`absolute inset-0 flex items-center justify-center z-10 transition-opacity duration-2000 ${
-            inView ? "opacity-100" : "opacity-0"
-          }`}
+       className={`absolute inset-0 flex flex-col justify-center z-10 transition-opacity duration-2000 ${
+            inView ? "opacity-100" : "opacity-0"}
+          
+     pt-[10%] pb-[15%] md:pt-[8%] md:pb-[10%]`}
+          
         >
-          <h1 className="text-center text-2xl md:text-6xl sm:text-5xl text-[#4E3D28] font-cursive   px-4">
+          <h1 className="text-center text-2xl sm:text-4xl md:text-6xl text-[#4E3D28] font-cursive px-4">
             Light your life with our Candles
           </h1>
         </div>
