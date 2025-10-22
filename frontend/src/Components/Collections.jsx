229a9f32e06c5004bc4a3a31c2a943cc303e3772
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-10-22 21:24:17 +0530

    Optimized the Collection section to handle the mobile view.

--- frontend/src/Components/Collections.jsx
+++ frontend/src/Components/Collections.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Collections } from "../Utility/data";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Collection() {
   const [toggleMenu, settoggleMenu] = useState(false);
@@ -17,11 +18,15 @@ export default function Collection() {
       [index]: !prev[index],
     }));
   };
+  const handleClick = () => {
+    navigate("/products");
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <>
       <section className="pb-4" id="products">
-        <div className="pb-10 relative px-8 bg-[linear-gradient(to_top_right,#ffba52_0%,#ffffff_50%,#ffba52_100%)] border-4 border-white w-full h-auto rounded-2xl">
+        <div className="pb-8 relative px-8 bg-[linear-gradient(to_top_right,#ffba52_0%,#ffffff_50%,#ffba52_100%)] border-4 border-white w-full h-auto rounded-2xl">
           <div className="flex justify-between items-center mt-1">
             <h1
               className="relative inline-block pb-2 md:pb-5 mt-5 text-lg md:text-2xl font-hero text-[#4E3D28] underline-on-scroll"
@@ -30,8 +35,10 @@ export default function Collection() {
             >
               COLLECTIONS
             </h1>
-            <h1
-              className="cursor-pointer font-semibold relative z-30 px-4 py-2 mt-5 text-xl font-hero text-[#4E3D28] floatlr-img shadow-2xl bg-[#f1cc94] hover:bg-white hover:border-[#ffce85] rounded-[300px] border-4 border-white"
+                <Link
+              onClick={handleClick}
+              to="/products"
+              className="cursor-pointer font-semibold relative z-30 px-4 py-2 mt-5 text-sm md:text-xl font-hero text-[#4E3D28] floatlr-img shadow-2xl bg-[#f1cc94] hover:bg-white hover:border-[#ffce85] rounded-[300px] border-4 border-white"
               data-aos="fade"
               data-aos-once="false"
             >
@@ -42,7 +49,7 @@ export default function Collection() {
                 viewBox="0 0 24 24"
                 strokeWidth="2"
                 stroke="currentColor"
-                className="w-6 h-6 ml-2 text-black inline"
+                className="w-3 h-3 md:w-6 md:h-6 ml-2 text-black inline"
               >
                 <path
                   strokeLinecap="round"
@@ -50,7 +57,7 @@ export default function Collection() {
                   d="M11.25 4.5l7.5 7.5-7.5 7.5M4.5 4.5l7.5 7.5-7.5 7.5"
                 />
               </svg>
-            </h1>
+            </Link>
           </div>
 
           <div className="grid grid-cols-2 md:flex md:flex-wrap justify-center gap-4 md:gap-6 rounded-2xl">
@@ -74,9 +81,9 @@ export default function Collection() {
                     }`}
                   />
 
-                  <div className="hidden md:block absolute bottom-0 w-full bg-[#4E3D28]/80 text-white flex justify-between items-center px-4 py-2 text-xl font-semibold opacity-100 transition-opacity duration-700">
+                  <div className=" md:block absolute bottom-0 w-full bg-[#4E3D28]/80 text-white flex justify-between items-center px-4 py-2 text-sm md:text-xl font-semibold opacity-100 transition-opacity duration-700">
                     <span>{item.name}</span>
-                    <button className="bg-red-400 p-2 rounded-xl cursor-pointer hover:bg-white hover:text-red-400 font-bold">
+                    <button className="bg-red-400 p-2 rounded-xl cursor-pointer hover:bg-white hover:text-red-400 font-bold hidden md:block ">
                         shop now
 
                     </button>
