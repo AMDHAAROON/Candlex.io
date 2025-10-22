30324c033796ac81b9b895bd3bf76b94743d8d9b
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-10-22 21:21:16 +0530

    Optimized the hand picked offers section to handle the mobile view

--- frontend/src/Components/Product.jsx
+++ frontend/src/Components/Product.jsx
@@ -36,7 +36,7 @@ export default function Product() {
               HANDPICKED OFFERS
             </h1>
             <Link
-               onClick={handleClick}
+              onClick={handleClick}
               to="/products"
               className="cursor-pointer font-semibold relative z-30 px-4 py-2 mt-5 text-sm md:text-xl font-hero text-[#4E3D28] floatlr-img shadow-2xl bg-[#f1cc94] hover:bg-white hover:border-[#ffce85] rounded-[300px] border-4 border-white"
               data-aos="fade"
@@ -60,44 +60,43 @@ export default function Product() {
             </Link>
           </div>
 
-      <div className="grid grid-cols-2 justify-items-center md:flex md:flex-wrap justify-center gap-4 md:gap-6 pt-4">
-  {candleProducts.map((item, index) => (
-    <div
-      key={index}
-        data-aos="fade-up"
-  data-aos-delay={index * 200}
-      className={`group relative my-4 rounded-2xl overflow-hidden h-[250px] w-[160px] md:h-[600px] md:w-[400px] transform transition-transform duration-700 ease-in-out group-hover:scale-105 shadow-2xl
+          <div className="grid grid-cols-2 justify-items-center md:flex md:flex-wrap justify-center gap-4 md:gap-6 pt-4">
+            {candleProducts.map((item, index) => (
+              <div
+                key={index}
+                data-aos="fade-up"
+                data-aos-delay={index * 200}
+                className={`group relative my-4 rounded-2xl overflow-hidden h-[250px] w-[160px] md:h-[600px] md:w-[400px] transform transition-transform duration-700 ease-in-out group-hover:scale-105 shadow-2xl
         ${index === 2 ? "col-span-2 justify-self-center" : ""}
       `}
-    >
-      <img
-        src={item.image}
-        alt={item.name}
-        className="rounded-2xl h-[250px] w-full md:h-[600px] object-cover transform transition-transform duration-700 ease-in-out group-hover:scale-105"
-      />
-      <div className="absolute bottom-0 w-full bg-[#4E3D28]/80 text-white flex justify-between items-center px-2 py-1 md:px-4 md:py-2 text-sm md:text-2xl font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-700">
-        <span>{item.name}</span>
-        <button onClick={() => toggleLike(index)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill={liked[index] ? "red" : "none"}
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-5 md:w-8 md:h-8 text-white transition-all duration-300"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 6.677c0-2.39-1.94-4.32-4.33-4.32-1.466 0-2.768.722-3.555 1.825A4.344 4.344 0 0 0 10.312 2.4c-2.39 0-4.33 1.93-4.33 4.32 0 6.234 7.158 10.197 7.158 10.197s7.158-3.963 7.158-10.197z"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
-  ))}
-</div>
-
+              >
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="rounded-2xl h-[250px] w-full md:h-[600px] object-cover transform transition-transform duration-700 ease-in-out group-hover:scale-105"
+                />
+                <div className="absolute bottom-0 w-full bg-[#4E3D28]/80 text-white flex justify-between items-center px-2 py-1 md:px-4 md:py-2 text-[12px] md:text-2xl font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-700">
+                  <span>{item.name}</span>
+                  <button onClick={() => toggleLike(index)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill={liked[index] ? "red" : "none"}
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5 md:w-8 md:h-8 text-white transition-all duration-300"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21.752 6.677c0-2.39-1.94-4.32-4.33-4.32-1.466 0-2.768.722-3.555 1.825A4.344 4.344 0 0 0 10.312 2.4c-2.39 0-4.33 1.93-4.33 4.32 0 6.234 7.158 10.197 7.158 10.197s7.158-3.963 7.158-10.197z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </>
