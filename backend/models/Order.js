e28fa76665e0760fd80fd9f6f0baf2d2dfe4a634
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-11-14 22:09:02 +0530

    Updated the order collection schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 
+// order.js schema
 const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  productName: String,
-  quantity: Number,
-  price: Number,
+  userId: { type: String, required: true },
+  userName: { type: String, required: true },
+  products: [
+    {
+      productId: { type: String, required: true }, // ✅ string
+      productName: String,
+      price: Number,
+      qty: Number,
+    }
+  ],
+  totalAmount: Number,
   orderDate: { type: Date, default: Date.now },
 });
+
 
 export default mongoose.model("Order", orderSchema);
