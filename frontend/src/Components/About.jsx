7c955dc1ab5712233aae49e7e504a6a05fa5dc83
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-11-16 19:27:35 +0530

    Refactor About component layout and update YouTube iframe attributes

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -8,7 +8,10 @@ export default function About() {
   }, []);
 
   return (
-    <section id="About" className="rounded-2xl border-2 border-white w-full  py-12 bg-gradient-to-b from-amber-50 via-yellow-50 to-white">
+    <section
+      id="About"
+      className="rounded-2xl border-2 border-white w-full  py-12 bg-gradient-to-b from-amber-50 via-yellow-50 to-white"
+    >
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-8">
         {/* Left: YouTube Video */}
         <div
@@ -17,26 +20,33 @@ export default function About() {
         >
           <iframe
             className="w-full h-full rounded-2xl  border-4 border-white"
-            src="https://www.youtube.com/embed/j_P35HJn5Ow?autoplay=1&mute=1&controls=1"
-            title="About Candle Brand"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/j_P35HJn5Ow?si=nrbxQx5Ezi9WovV5"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
           ></iframe>
         </div>
 
         {/* Right: About Note */}
         <div className="w-full md:w-1/2" data-aos="fade-left">
-        <div className="max-w-7xl mx-auto mb-4">
-          <h2 className="text-4xl  font-bold text-amber-700 pb-4">OUR MAKINGS</h2>
+          <div className="max-w-7xl mx-auto mb-4">
+            <h2 className="text-4xl  font-bold text-amber-700 pb-4">
+              OUR MAKINGS
+            </h2>
           </div>
           <p className="text-[#4E3D28] text-xl  font-mono leading-relaxed">
-            Welcome to our candle world 🔥<br/> 
-           Each candle is crafted with love using natural ingredients and soothing scents.
-We aim to light up your space with warmth, elegance, and a touch of calm.
-Hand-poured in small batches, our candles are made to comfort and inspire.
+            Welcome to our candle world 🔥
+            <br />
+            Each candle is crafted with love using natural ingredients and
+            soothing scents. We aim to light up your space with warmth,
+            elegance, and a touch of calm. Hand-poured in small batches, our
+            candles are made to comfort and inspire.
           </p>
-        
-      </div>
+        </div>
       </div>
     </section>
   );
